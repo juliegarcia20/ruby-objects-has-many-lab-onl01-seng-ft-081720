bf8e690609da972498f47e04065819d365dd0244
Julie Garcia <juliegarcia.cali@gmail.com>
2020-09-02 17:31:20 +0000

    Automatically backed up by Learn

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -16,14 +16,7 @@ describe "Author" do
       end
     end
 
-    describe "#posts" do
-      it "has many posts" do
-        expect(betty.posts).to be_a(Array)
-        post = Post.new("My Post")
-        post.author = betty
-        expect(betty.posts).to eq([post])
-      end
-    end
+    
 
     describe "#add_post" do
       it "takes in an argument of a post and associates that post with the author by telling the post that it belongs to that author" do
